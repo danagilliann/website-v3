cfbbed7187df31f7e2a631ca691b7ac3887c2768
danagilliann <danalee96@gmail.com>
2016-05-27 16:29:59 -0400

    process form

--- app/js/controllers/index-ctrl.js
+++ app/js/controllers/index-ctrl.js
@@ -3,8 +3,14 @@
 
     var app = angular.module("myApp", ["myApp.config"]);
 
-    app.controller("indexCtrl", function($scope, api_key) {
-        $scope.poo = api_key;
+    app.controller("indexCtrl", function($scope, api_key, $http) {
+        $scope.api_key = api_key;
+        $scope.color = "";
+
+        $scope.submit = function() {
+            console.log("poo");
+            :wa
+        }
     });
 
 })();
