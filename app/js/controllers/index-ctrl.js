aede080c166658b738a1e424dcccada47be87ef6
danagilliann <danalee96@gmail.com>
2016-05-27 23:18:26 -0400

    poopy success

--- app/js/controllers/index-ctrl.js
+++ app/js/controllers/index-ctrl.js
@@ -4,11 +4,17 @@
     var app = angular.module("myApp", ["myApp.config", "ngAnimate"]);
 
     app.controller("indexCtrl", function($scope, api_key, $http) {
-        $scope.api_key = api_key;
         $scope.color = "Enter a color";
 
         $scope.submit = function() {
-            console.log(true);
+            var url = "https://api.behance.net/v2/projects?callback=JSON_CALLBACK&color_hex=" + $scope.color + "&client_id=" + api_key;
+            
+            $http.jsonp(url)
+                .then(function success(res) {
+                    console.log(res);
+                }, function err(res) {
+                    console.log(url);
+            });
         };
     });
 
