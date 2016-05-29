84e8aa7035487e24ddfc49720dfcb1a0c7a07568
danagilliann <danalee96@gmail.com>
2016-05-29 14:42:03 -0400

    err goneeeee

--- app/js/controllers/index-ctrl.js
+++ app/js/controllers/index-ctrl.js
@@ -6,6 +6,12 @@
     app.controller("indexCtrl", function($scope, api_key, $http) {
         $scope.color = "Enter a color";
 
+        $scope.pictures = [{
+                covers: {
+                    original: ""
+                }
+        }];
+
         $scope.submit = function() {
             var url = "https://api.behance.net/v2/projects?callback=JSON_CALLBACK&color_hex=" + $scope.color + "&client_id=" + api_key;
 
