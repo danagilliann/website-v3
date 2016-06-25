669e5cec4aeac048556dc6bd89bf1376fa707b48
danagilliann <danalee96@gmail.com>
2016-06-25 00:46:53 -0400

    okay ladiez now let's get into formation

--- app/js/controllers/index-ctrl.js
+++ app/js/controllers/index-ctrl.js
@@ -3,25 +3,7 @@
 
     var app = angular.module("myApp", ["myApp.config", "ngAnimate"]);
 
-    app.controller("indexCtrl", function($scope, api_key, $http) {
-        $scope.color = "Enter a color";
-
-        $scope.pictures = [{
-                covers: {
-                    original: ""
-                }
-        }];
-
-        $scope.submit = function() {
-            var url = "https://api.behance.net/v2/projects?callback=JSON_CALLBACK&color_hex=" + $scope.color + "&client_id=" + api_key;
-
-            $http.jsonp(url)
-                .then(function success(res) {
-                    $scope.pictures = res.data.projects;
-                }, function err(res) {
-                    clickSubmit = false;
-            });
-        };
+    app.controller("indexCtrl", function($scope) {
     });
 
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,32 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var gulpNgConfig = require("gulp-ng-config");
-var server = require("gulp-server-livereload");
+var gulp = require('gulp');
+var gulpNgConfig = require('gulp-ng-config');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
 
-gulp.task("config", function() {
-    gulp.src("config.json")
-        .pipe(gulpNgConfig("myApp.config"))
-        .pipe(gulp.dest("."));
+gulp.task('default', ['serve','sass','sass:watch'], function () {
+
 });
 
-gulp.task("serve", function() {
-    gulp.src("./app")
+gulp.task('sass', function () {
+  return gulp.src('app/style/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/style/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('app/style/sass/*.sass', ['sass']);
+});
+
+gulp.task('serve', function () {
+
+    gulp.src('./app')
         .pipe(server({
             livereload: true,
             open: true,
-            defaultFile: "index.html",
+            defaultFile: 'index.html',
             directoryListing: false
         }));
+
 });
