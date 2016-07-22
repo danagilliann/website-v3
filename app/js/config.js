f1b4b93d28d7ea0d9632ce41d4b84443582d032c
danagilliann <danalee96@gmail.com>
2016-07-22 00:11:15 -0400

    cleanup

--- app/js/config.js
+++
@@ -1,2 +0,0 @@
-angular.module("myApp.config", [])
-.constant("api_key", "TPmJi6JfwMjIl8pRWDgXHaR47sr4srHl");
--- app/js/controllers/index-ctrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    var app = angular.module("myApp", ["myApp.config", "ngAnimate"]);
-
-    app.controller("indexCtrl", function($scope) {
-    });
-
-})();
