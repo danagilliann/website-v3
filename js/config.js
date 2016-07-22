c433b8f2ab778907695b0462fea735a87d8566b1
danagilliann <danalee96@gmail.com>
2016-07-21 23:57:35 -0400

    commit to website from v3

---
+++ js/config.js
@@ -0,0 +1,2 @@
+angular.module("myApp.config", [])
+.constant("api_key", "TPmJi6JfwMjIl8pRWDgXHaR47sr4srHl");
---
+++ js/controllers/index-ctrl.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    var app = angular.module("myApp", ["myApp.config", "ngAnimate"]);
+
+    app.controller("indexCtrl", function($scope) {
+    });
+
+})();
